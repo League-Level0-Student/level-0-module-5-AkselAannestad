2565b63ad3870b302a4d5435148538180d9a85a9
League Student <league@WTS14-3.attlocal.net>
2020-01-15 20:01:09 -0800

    5
    5

--- src/_01_prime_or_not/primeornot.java
+++ src/_01_prime_or_not/primeornot.java
@@ -1,5 +1,16 @@
 package _01_prime_or_not;
 
-public class primeornot {
+import javax.swing.JOptionPane;
 
-}
+public class primeornot {
+private void mian() {
+	// TODO Auto-generated method stub
+String x=JOptionPane.showInputDialog("enter a number and lets see if its prime");
+	int y=Integer.parseInt(x);
+	for (int i = 0; i < i+1; i++) {
+		}
+if(y%2==0) {System.out.println("");}
+
+
+
+}}

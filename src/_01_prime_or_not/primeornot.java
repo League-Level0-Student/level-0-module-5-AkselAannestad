f9acd0f4fbb4589bc16cf86da19e06b0001e297c
League Student <league@iMac-55.attlocal.net>
2020-01-29 19:59:58 -0800

    logic
    logic = math but not really

--- src/_01_prime_or_not/primeornot.java
+++ src/_01_prime_or_not/primeornot.java
@@ -3,14 +3,15 @@ package _01_prime_or_not;
 import javax.swing.JOptionPane;
 
 public class primeornot {
-private void mian() {
+public static void main(String [] args) {
 	// TODO Auto-generated method stub
-String x=JOptionPane.showInputDialog("enter a number and lets see if its prime");
+
+	String x=JOptionPane.showInputDialog("enter a number and lets see if its prime");
 	int y=Integer.parseInt(x);
-	for (int i = 0; i < i+1; i++) {
-		}
-if(y%2==0) {System.out.println("");}
+	for (int i = 2; i < y; i++) {
+		if(y%i==0) {System.out.println("This number is not prime");}
+		else {System.out.println("This number is prime");}
+	}
 
-
-
-}}
+}
+}
---
+++ src/_02_fibonacci/fibonaccijava.java
@@ -0,0 +1,16 @@
+package _02_fibonacci;
+
+public class fibonaccijava {
+public static void main(String[] args) {
+	int x;
+	int y;
+x=1; 
+System.out.println(x);
+
+	
+	
+
+	
+	
+// 0 1 1 2 3 5 8 13 21 34 55 89
+}}

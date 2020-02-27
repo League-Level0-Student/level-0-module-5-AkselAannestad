0402b366a3f14b62b164cb30797978c8f9b53172
League Student <league@iMac-94.attlocal.net>
2020-02-26 19:58:16 -0800

    sad
    serious art disorder

--- src/_02_fibonacci/fibonaccijava.java
+++ src/_02_fibonacci/fibonaccijava.java
@@ -6,10 +6,16 @@ public static void main(String[] args) {
 	int y;
 	int z;
 x=0; 
-y=1;
+y=0;
+z=0;
 	for(int i=0;i<12;i++) {
-		x=x+y;
+		System.out.println(z);
+		y=x;
+		x+=1;
+		z=x+=y;
+	
 		
+	
 	}
 
 	

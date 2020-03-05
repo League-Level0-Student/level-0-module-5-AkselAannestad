fda1eff04b48cb6b839baf7eebb42c4273805368
League Student <league@iMac-16.attlocal.net>
2020-03-04 19:58:45 -0800

    checkers
    checkers

--- src/_06_for_loop_gauntlet/Forloopgauntlet.java
+++ src/_06_for_loop_gauntlet/Forloopgauntlet.java
@@ -27,10 +27,32 @@ public static void main(String[] args) {
 	}
 
 //odd from 1-99
+	int a=0;
+	for(int i =0; i<50; i++) {
+		a+=1;
+		//System.out.println(a);
+		a+=1;
+	}
+//1-500 odd or even
 	
-	
-	
-	
+	for(int i =1; i<501; i++) {
+		//if(i%2==0) {System.out.println(i+" is even");}
+		//else {System.out.println(i+" is odd");}
+	}
+//all multiples of 7 from 0-777
+	int b=0;
+	for (int i = 0; i <112; i++) {
+		//System.out.println(b);
+		b=b+7;
+	}
+//Years i was alive
+	int year=2004;
+	int years=0;
+	for (int i = 0; i<16; i++) {
+		System.out.println("In "+year+" I was "+years+" years old");
+		years=years+1;
+		year=year+1;
+	}
 	
 
 }}

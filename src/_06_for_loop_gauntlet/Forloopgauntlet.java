2b492be279b876acad1c458d44890dc9dd56710a
League Student <league@iMac-13.attlocal.net>
2020-03-08 12:26:27 -0700

    forloooooopgauntlet
    kid next to me thought i was level 3 lmao

--- src/_06_for_loop_gauntlet/Forloopgauntlet.java
+++ src/_06_for_loop_gauntlet/Forloopgauntlet.java
@@ -53,6 +53,42 @@ public static void main(String[] args) {
 		years=years+1;
 		year=year+1;
 	}
-//
+//Nested for loops
+	
+	int o=0;
+	for(int i=0;i<3;i++) {
+		for(int p=0;p<3;p++){
+		//System.out.println(o+"  "+p);
+	
+	}
+		o+=1;}
+	//1-9 in 3x3 grid
+	int q=1;
+	for(int i=0;i<3;i++) {
+		for (int j = 0; j < 3; j++) {
+			int w=q+1;
+			int e=q+2;
+			//System.out.println(q+"  "+w+"  "+e);
+			q=q+=3;
+		}
+	}
+	//1=100 10x10 grid
+	for (int i = 0; i <10; i++) {
+		for (int j = 0; j < 10; j++) {
+			
+			
+			
+			
+		}
+		
+	}
+	
+	
+
+	
+	
+
+	
+
 
 }}

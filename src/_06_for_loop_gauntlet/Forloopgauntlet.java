a40ecf24aa3421074ea882992a926ded51f208fe
League Student <league@iMac-16.attlocal.net>
2020-03-11 19:58:51 -0700

    f
    f

--- src/_06_for_loop_gauntlet/Forloopgauntlet.java
+++ src/_06_for_loop_gauntlet/Forloopgauntlet.java
@@ -88,7 +88,7 @@ public static void main(String[] args) {
 			System.out.print("* ");
 		}
 	
-		System.out.println("");
+		//System.out.println("");
 	}
 	
 	
--- src/_07_skill_practice/skillspractice.java
+++ src/_07_skill_practice/skillspractice.java
@@ -1,5 +1,9 @@
 package _07_skill_practice;
 
 public class skillspractice {
+public static void main(String[] args) {
+	SkillPractice skills= new SkillPractice();
+	skills.skill1();
 
-}
+
+}}

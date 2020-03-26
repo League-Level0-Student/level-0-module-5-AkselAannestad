83b0a420903e38190f886ee68ed1d7a9a81aede7
AkselAannestad <48100535+AkselAannestad@users.noreply.github.com>
2020-03-25 19:58:40 -0700

    at home
    coronavirus

--- src/_07_skill_practice/skillspractice.java
+++ src/_07_skill_practice/skillspractice.java
@@ -1,9 +1,20 @@
 package _07_skill_practice;
 
+import javax.swing.JOptionPane;
+
 public class skillspractice {
 public static void main(String[] args) {
-	SkillPractice skills= new SkillPractice();
-	skills.skill1();
-
-
+	skillspractice skills= new skillspractice();
+		skills.skill1();
+		skills.skill2();
+		skills.skill3();
+		skills.skill4();
+		skills.skill5();
+		
+ void skill1() {
+	String dimes=JOptionPane.showInputDialog("how many dimes do you have?");
+	int dimenum=Integer.parseInt(dimes);
+		
+ }
+ 
 }}

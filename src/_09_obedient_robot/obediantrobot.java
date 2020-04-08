033d5d1398064c3bb040c46950bda654ee2e276d
AkselAannestad <48100535+AkselAannestad@users.noreply.github.com>
2020-04-07 21:01:25 -0700

    home
    corona

---
+++ src/_09_obedient_robot/obediantrobot.java
@@ -0,0 +1,20 @@
+package _09_obedient_robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+
+public class obediantrobot {
+	
+public static void main(String[] args) throws AWTException {
+	
+	
+}
+
+public static void drawsquare() {
+	Robot bruh= new Robot();
+	bruh.speed(10);
+	bruh.forward(20);
+	
+}
+
+}

91aeb574be8cc70ec1b90911ae6edc2236e28b23
AkselAannestad <48100535+AkselAannestad@users.noreply.github.com>
2020-04-08 19:56:40 -0700

    home
    home

--- src/_09_obedient_robot/obediantrobot.java
+++ src/_09_obedient_robot/obediantrobot.java
@@ -1,20 +1,85 @@
 package _09_obedient_robot;
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class obediantrobot {
+	Robot rob=new Robot();
+public static void main(String[] args)  {
+	int r;
+	int g;
+	int b;
+	String ask=JOptionPane.showInputDialog("do you want a square(1), triangle(2), or circle(3)?");
+	int shape=Integer.parseInt(ask);
 	
-public static void main(String[] args) throws AWTException {
 	
-	
+	if (shape==1) {drawsquare();}
+	else if(shape==2) {drawtriangle();}
+	else if(shape==3) {drawcircle();}
+	else {JOptionPane.showMessageDialog(null, "Error. Please enter one of the possible numbers given ");}
 }
 
 public static void drawsquare() {
-	Robot bruh= new Robot();
-	bruh.speed(10);
-	bruh.forward(20);
+	Robot rob= new Robot();
+	rob.penDown();
+	rob.setSpeed(100);
+	int r;
+	int g;
+	int b;
+	String ask2=JOptionPane.showInputDialog("Do you want your shape to be red(1), green(2), or blue(3)");
+	int color=Integer.parseInt(ask2);
 	
+	if(color==1) {rob.setPenColor(255,0,0);}
+	else if(color==2) {rob.setPenColor(0,255,0);}
+	else if(color==3) {rob.setPenColor(0,0,255);}
+	else {JOptionPane.showMessageDialog(null, "Error. Please enter one of the possible numbers");}
+	
+	for(int i=0;i<4;i++) {
+	rob.move(100);
+	rob.turn(90);}
+}
+
+
+public static void drawtriangle() {
+	Robot rob= new Robot();
+	rob.penDown();
+	rob.setSpeed(100);
+	int r;
+	int g;
+	int b;
+	String ask2=JOptionPane.showInputDialog("Do you want your shape to be red(1), green(2), or blue(3)");
+	int color=Integer.parseInt(ask2);
+	
+	if(color==1) {rob.setPenColor(255,0,0);}
+	else if(color==2) {rob.setPenColor(0,255,0);}
+	else if(color==3) {rob.setPenColor(0,0,255);}
+	else {JOptionPane.showMessageDialog(null, "Error. Please enter one of the possible numbers");}
+	
+	for(int i=0; i<3;i++) {
+	rob.move(100);
+	rob.turn(120);
+	}	
+}
+public static void drawcircle() {
+	Robot rob= new Robot();
+	rob.penDown();
+	rob.setSpeed(100);
+	int r;
+	int g;
+	int b;
+	String ask2=JOptionPane.showInputDialog("Do you want your shape to be red(1), green(2), or blue(3)");
+	int color=Integer.parseInt(ask2);
+	
+	if(color==1) {rob.setPenColor(255,0,0);}
+	else if(color==2) {rob.setPenColor(0,255,0);}
+	else if(color==3) {rob.setPenColor(0,0,255);}
+	else {JOptionPane.showMessageDialog(null, "Error. Please enter one of the possible numbers");}
+	
+	for(int i=0;i<360;i++) {
+		rob.move(1);
+		rob.turn(1);
+	}
 }
 
 }

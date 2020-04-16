18c686b5ce1a32909dd8e0658c3452d32246ae8c
AkselAannestad <48100535+AkselAannestad@users.noreply.github.com>
2020-04-15 19:57:50 -0700

    home again
    spcae and france

---
+++ src/houses.java
@@ -0,0 +1,21 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class houses {
+	
+public static void main(String[] args) {
+	Robot r=new Robot();
+	r.setSpeed(100000);
+	r.turn(-90);
+	r.move(350);
+	r.turn(-90);
+	r.move(100);
+    r.turn(-90);
+
+
+
+}
+
+
+
+
+}
